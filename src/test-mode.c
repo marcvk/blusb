f1fdf628ea9463f7f791394e236bb3cbfe258ce4
Marc van Kempen <marc@bowtie.nl>
2019-07-22 18:56:19 +0200

    - some changes to test-mode that mysteriously make it work ...

--- src/test-mode.c
+++ src/test-mode.c
@@ -3,11 +3,17 @@
 #include <time.h>
 #include <libusb.h>
 
+#include "usb.h"
+
 int main (int argc, char **argv) {
     if (argc == 2) {
         int mode = atoi(argv[1]);     
         if (bl_usb_openctrl()) {
             bl_usb_set_mode(mode);
+            int mode = bl_usb_get_mode();
+            printf("mode=%d\n", mode);
+            bl_usb_set_numlock(1);
+            bl_usb_closectrl();
         }
     } else {
         printf("usage: %s <0 | 1>\n", argv[0]);
